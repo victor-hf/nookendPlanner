039a279f5fcf9c73621c52520ade5c4e0b6e3f45
v3kth0r <v.herrero.fernandez@gmail.com>
2022-07-19 21:16:52 +0200

    Added Chore class and created test chores

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Goodbye Weeknd. Have fun with your chores!")
+console.log("Say goodbye to your Weeknd! Have fun with your chores!");
+
+const Chore = require('./helper.js');
+
+const chores = [];
+
+const testChore = new Chore('Recycling', 'Empty recicling bins', 'Weekly');
+chores.push(testChore);
+
+const testChore2 = new Chore('Vacuum appartment', 'Vacuum all common areas and bedroom', 'Monthly');
+chores.push(testChore2);
+
+
+console.log(chores);
---
+++ helper.js
@@ -0,0 +1,10 @@
+class Chore {
+    constructor(title, description, frequency) {
+        this.title = title;
+        this.description = description;
+        this.frequency = frequency;
+    } 
+
+};
+
+module.exports = Chore;
