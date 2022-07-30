58460718e26f81c03d68924252e89593afe42b6b
victor-hf <v.herrero.fernandez@gmail.com>
2022-07-30 17:11:54 +0200

    Renamed files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("Say goodbye to your Weeknd! Have fun with your chores!");
 
-const hlp = require("./helper.js");
-const InstanceOfChores = new hlp.Chores();
+const utils = require("./utils.js");
+const InstanceOfChores = new utils.Chores();
 
 InstanceOfChores.addChore(
   "Vacuum appartment",
