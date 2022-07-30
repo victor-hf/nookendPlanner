54ffa3c69ea5fe084ec96f1825a3e64bf745b7ae
victor-hf <v.herrero.fernandez@gmail.com>
2022-07-30 18:09:05 +0200

    Added completion method to chores class

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-console.log("Say goodbye to your Weeknd! Have fun with your chores!");
+console.log("Say goodbye to your Weeknd! \nHave fun with your chores!");
 
 const utils = require("./utils.js");
 const Chores = new utils.Chores();
+
+Chores.addChore(
+  "Dishes",
+  "Put dishes in dishwasher",
+  "Monthly"
+);
 
 Chores.addChore(
   "Vacuum appartment",
   "Vacuum all common areas and bedroom",
   "Monthly"
 );
+
 Chores.logChores();
+
+Chores.completeChore("Vacuum appartment")
+
+Chores.logChores();
+
+console.log(Chores.getChoresList())
